ba79db274d0f5062d1ccf1da2c5f8bd60f172bee
Evren Ekici <95691225+evrnekc@users.noreply.github.com>
2022-11-29 10:04:17 +0300

    Create odev6.sql

---
+++ odev6.sql
@@ -0,0 +1,13 @@
+-- for explanation see README.md
+-- açıklamalar için README.md dosyasına göz atabilirsiniz
+
+SELECT AVG(rental_rate) FROM film;
+--
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'C%';
+--
+SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99;
+--
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150;
